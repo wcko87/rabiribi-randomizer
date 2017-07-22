09d8c2eedb31926739f589e25da88080465e15ee
wcko87 <wcko87@gmail.com>
2017-07-22 13:32:56 +0800

    initial commit. it's working

---
+++ itemrandomizer.py
@@ -0,0 +1,394 @@
+import random
+import re
+import json
+import itemreader
+import sys
+
+def define_variables(item_names):
+    variables = {
+        "TRUE": True,
+        "FALSE": False,
+        "ZIP_REQUIRED": False,
+        "POST_GAME_ALLOWED": True,
+        "POST_IRISU_ALLOWED": True,
+        "STUPID_HARD_TRICKS": False,
+    }
+    for item_name in item_names:
+        variables[item_name] = False
+    return variables
+
+def define_custom_items():
+    # Generally used for items that require you to exit to the shop before you can fully utilize it
+    return {
+        "WALL_JUMP_LV2": {
+            "entry_prereq": "WALL_JUMP",
+            "exit_prereq": "NONE",
+        },
+        "HAMMER_ROLL_LV3": {
+            "entry_prereq": "HAMMER_ROLL",
+            "exit_prereq": "NONE",
+        },
+        "BUNNY_STRIKE": {
+            "entry_prereq": "SLIDING_POWDER",
+            "exit_prereq": "NONE",
+        },
+    }
+
+def define_default_expressions(variables):
+    # Default expressions take priority over actual variables.
+    # so if we parse an expression that has AIR_DASH, the default expression AIR_DASH will be used instead of the variable AIR_DASH.
+    # however, the expressions parsed in define_default_expressions (just below) cannot use default expressions in their expressions.
+    return {
+        "ZIP": parse_expression("ZIP_REQUIRED", variables),
+        "POST_GAME": parse_expression("POST_GAME_ALLOWED", variables),
+        "STUPID": parse_expression("STUPID_HARD_TRICKS", variables),
+        "POST_IRISU": parse_expression("POST_IRISU_ALLOWED", variables),
+        "BUNNY_STRIKE": parse_expression("BUNNY_STRIKE & PIKO_HAMMER", variables),
+        "BUNNY_WHIRL": parse_expression("BUNNY_WHIRL & PIKO_HAMMER", variables),
+        "AIR_DASH": parse_expression("AIR_DASH & PIKO_HAMMER", variables),
+        "HAMMER_ROLL_LV3": parse_expression("HAMMER_ROLL_LV3 & PIKO_HAMMER", variables),
+        "BOOST": parse_expression("TRUE", variables),
+        #"RIBBON": parse_expression("TRUE", variables),
+        #"WARP": parse_expression("TRUE", variables),
+        #"EXIT_PROLOGUE": parse_expression("TRUE", variables),
+        "TRUE": parse_expression("TRUE", variables),
+        "FALSE": parse_expression("FALSE", variables),
+        "NONE": parse_expression("TRUE", variables),
+        "IMPOSSIBLE": parse_expression("FALSE", variables),
+    }
+
+def fail(message):
+    print(message)
+    sys.exit(1)
+
+# Used in string parsing. We only have either strings or expressions
+isExpr = lambda s : not type(s) is str
+
+# & - and
+# | - or
+# !/~ - not
+# ( ) - parentheses
+# throws errors if parsing fails
+def parse_expression(line, variables, default_expressions={}):
+    try:
+        return parse_expression_logic(line, variables, default_expressions)
+    except Exception as e:
+        print('Error parsing expression:')
+        print(line)
+        raise e
+
+def parse_expression_logic(line, variables, default_expressions):
+    pat = re.compile('[()&|~!]')
+    line = line.replace('&&', '&').replace('||', '|')
+    tokens = (s.strip() for s in re.split('([()&|!~])', line))
+    tokens = [s for s in tokens if s]
+    # Stack-based parsing. pop from [tokens], push into [stack]
+    # We push an expression into [tokens] if we want to process it next iteration.
+    tokens.reverse()
+    stack = []
+    while len(tokens) > 0:
+        next = tokens.pop()
+        if isExpr(next):
+            if len(stack) == 0:
+                stack.append(next)
+                continue
+            head = stack[-1]
+            if head == '&':
+                stack.pop()
+                exp = stack.pop()
+                assert isExpr(exp)
+                tokens.append(OpAnd(exp, next))
+            elif head == '|':
+                stack.pop()
+                exp = stack.pop()
+                assert isExpr(exp)
+                tokens.append(OpOr(exp, next))
+            elif head in '!~':
+                stack.pop()
+                tokens.append(OpNot(next))
+            else:
+                stack.append(next)
+        elif next in '(&|!~':
+            stack.append(next)
+        elif next == ')':
+            exp = stack.pop()
+            assert isExpr(exp)
+            assert stack.pop() == '('
+            tokens.append(exp)
+        else: # string literal
+            if next in default_expressions:
+                tokens.append(default_expressions[next])
+            elif next not in variables:
+                fail('Unknown variable %s in expression: %s' % (next, line))
+            else:
+                tokens.append(OpLit(next))
+    assert len(stack) == 1
+    return stack[0]
+
+
+class OpLit(object):
+    def __init__(self, name):
+        self.name = name
+    def evaluate(self, variables):
+        return variables[self.name]
+    def __str__(self):
+        return self.name
+    __repr__ = __str__
+
+class OpNot(object):
+    def __init__(self, expr):
+        self.expr = expr
+    def evaluate(self, variables):
+        return not self.expr.evaluate(variables)
+    def __str__(self):
+        return '(NOT %s)' % self.expr
+    __repr__ = __str__
+
+class OpOr(object):
+    def __init__(self, exprL, exprR):
+        self.exprL = exprL
+        self.exprR = exprR
+    def evaluate(self, variables):
+        return self.exprL.evaluate(variables) or self.exprR.evaluate(variables)
+    def __str__(self):
+        return '(%s OR %s)' % (self.exprL, self.exprR)
+    __repr__ = __str__
+
+class OpAnd(object):
+    def __init__(self, exprL, exprR):
+        self.exprL = exprL
+        self.exprR = exprR
+    def evaluate(self, variables):
+        return self.exprL.evaluate(variables) and self.exprR.evaluate(variables)
+    def __str__(self):
+        return '(%s AND %s)' % (self.exprL, self.exprR)
+    __repr__ = __str__
+
+
+def read_file_and_strip_comments(filename):
+    def strip_comments(line):
+        if '//' not in line: return line
+        return line[:line.find('//')]
+    with open(filename) as f:
+        lines = [strip_comments(line).strip() for line in f]
+    return lines
+
+def print_error(error, jsondata):
+    import re
+    pos = int(re.findall('char ([\\d]*)', error.__str__())[0])
+    VIEW_RANGE = 100
+    start = max(pos-VIEW_RANGE, 0)
+    end = min(pos+VIEW_RANGE, len(jsondata))
+    print('Parsing error')
+    print(error)
+    print('Error location:')
+    print(jsondata[start:pos])
+    print(jsondata[pos:end])
+
+# throws errors for invalid formats.
+# returns a dict mapping each location to its prereqs.
+def read_constraints(locations, variables, default_expressions, custom_items):
+    lines = read_file_and_strip_comments('constraints.txt')
+    jsondata = ' '.join(lines)
+    jsondata = re.sub(',\s*}', '}', jsondata)
+    jsondata = '},{'.join(re.split('}\s*{', jsondata))
+    jsondata = '[' + jsondata + ']'
+    try:
+        cdicts = json.loads(jsondata)
+    except ValueError as e:
+        print_error(e, jsondata)
+        sys.exit(1)
+
+    DEFAULT_CONSTRAINT = Constraint(default_expressions['IMPOSSIBLE'], default_expressions['IMPOSSIBLE'])
+    locations_set = set(locations)
+    constraints = dict((location, DEFAULT_CONSTRAINT) for location in locations_set)
+    for cdict in cdicts:
+        assert cdict['location'] in locations_set
+        entry_expression = parse_expression(cdict['entry_prereq'], variables, default_expressions)
+        exit_expression = parse_expression(cdict['exit_prereq'], variables, default_expressions)
+        constraints[cdict['location']] = Constraint(entry_expression, exit_expression)
+
+    for item_name, cdict in custom_items.items():
+        assert item_name in locations_set
+        entry_expression = parse_expression(cdict['entry_prereq'], variables, default_expressions)
+        exit_expression = parse_expression(cdict['exit_prereq'], variables, default_expressions)
+        constraints[item_name] = Constraint(entry_expression, exit_expression)
+
+    return constraints
+
+def read_config(variables, item_names):
+    lines = read_file_and_strip_comments('config.txt')
+    config_dict = json.loads('{' + ' '.join(lines) + '}')
+
+    to_shuffle = config_dict['to_shuffle']
+    must_be_reachable = set(config_dict['must_be_reachable'])
+    settings = config_dict['settings']
+    for key, value in settings.items():
+        if key not in variables:
+            fail('Undefined variable: %s' % key)
+        if not type(value) is bool:
+            fail('Variable %s does not map to a boolean variable in config.txt' % key)
+        variables[key] = value
+
+    if set(to_shuffle) - set(item_names):
+        fail('\n'.join[
+            'Unknown items defined in config:',
+            '\n'.join(map(str, set(to_shuffle) - set(item_names))),
+        ])
+
+    if set(must_be_reachable) - set(item_names):
+        fail('\n'.join[
+            'Unknown items defined in config:',
+            '\n'.join(map(str, set(must_be_reachable) - set(item_names))),
+        ])
+
+    return to_shuffle, must_be_reachable
+
+def read_items():
+    lines = read_file_and_strip_comments('all_items.txt')
+    items = [itemreader.parse_item_from_string(line)
+             for line in lines if line.lstrip().startswith('(')]
+    return items
+
+
+def load_everything():
+    items = read_items()
+    custom_items = define_custom_items()
+    locations = [item.name for item in items] + list(custom_items.keys())
+    item_names = locations
+    variables = define_variables(item_names)
+    default_expressions = define_default_expressions(variables)
+
+    to_shuffle, must_be_reachable = read_config(variables, item_names)
+    constraints = read_constraints(locations, variables, default_expressions, custom_items)
+
+    randomize(items, locations, variables, to_shuffle, must_be_reachable, constraints)
+
+
+class Constraint(object):
+    def __init__(self, entry_expression, exit_expression):
+        self.entry_expression = entry_expression
+        self.exit_expression = exit_expression
+
+    def can_enter(self, variables):
+        return self.entry_expression.evaluate(variables)
+
+    def can_exit(self, variables, current_item_name):
+        temp = variables[current_item_name]
+        variables[current_item_name] = True
+        result = self.exit_expression.evaluate(variables)
+        variables[current_item_name] = temp
+        return result
+
+    def can_enter_and_exit(self, variables, current_item_name):
+        return self.can_enter(variables) and \
+               self.can_exit(variables, current_item_name)
+
+    def __str__(self):
+        return '\n'.join([
+            'Entry: %s' % self.entry_expression,
+            'Exit: %s' % self.exit_expression,
+        ])
+
+class LocationMap(object):
+    def __init__(self, items, locations, variables, constraints):
+        # name -> item mapping
+        self.items = dict((item.name, item) for item in items)
+        # default assignment assigns every item to its original location.
+        self.assigned_locations = dict((location, location) for location in locations)
+        self.locations = locations
+        self.variables = dict(variables) # copy
+        self.constraints = constraints
+
+    def reassign(self, assignments):
+        for location, item_name in assignments:
+            self.assigned_locations[item_name] = location
+
+    def validate_required_reachables(self, must_be_reachable):
+        unreachable = self.compute_unreachable()
+        print(unreachable)
+        quit()
+        return len(must_be_reachable & unreachable) == 0
+
+    def compute_unreachable(self):
+        unreachable = set(self.locations) # copy
+        to_remove = set()
+        variables = dict(self.variables) # copy
+        has_change = True
+
+        while has_change:
+            has_change = False
+            for item_name in unreachable:
+                if self.constraints[item_name].can_enter_and_exit(variables, item_name):
+                    # item can be reached.
+                    to_remove.add(item_name)
+                    has_change = True
+            for item_name in to_remove:
+                variables[item_name] = True
+            print('---- NEXT PHASE ----')
+            print(to_remove)
+            unreachable -= to_remove
+            to_remove.clear()
+        return unreachable
+
+    def compute_item_locations(self):
+        new_items = []
+        for item_name, location in self.assigned_locations.items():
+            if item_name not in self.items: continue # custom items are skipped
+            new_item = self.items[item_name].copy()
+            new_item.set_location(self.items[location])
+            new_items.append(new_item)
+        return new_items
+
+
+def randomize(items, locations, variables, to_shuffle, must_be_reachable, constraints):
+    items = list(items) # all actual items
+    locations = list(locations) # actual items + custom items
+    must_be_reachable = set(must_be_reachable)
+
+    # shuffling
+    to_shuffle = list(to_shuffle)
+    target_locations = to_shuffle[:]
+
+    location_map = LocationMap(items, locations, variables, constraints)
+    attempts = 0
+    while True:
+        attempts += 1
+        random.shuffle(target_locations)
+        location_map.reassign(zip(target_locations, to_shuffle))
+        if location_map.validate_required_reachables(must_be_reachable): break
+
+    print('Computed after %d attempts' % attempts)
+    return location_map.compute_item_locations()
+
+
+if __name__ == '__main__':
+    load_everything()
+    quit()
+
+    locations = ['FOO', 'BAR', 'BAND', 'SLIDING_POWDER']
+    variables = define_variables(locations)
+    default_expressions = define_default_expressions(variables)
+    constraints = read_constraints(locations, variables, default_expressions)
+    for key, value in constraints.items():
+        print('Key: %s' % key)
+        print('Value: %s' % value)
+        print('Enter Result: %s' % value.can_enter(variables))
+        print('Exit Result: %s' % value.can_exit(variables, key))
+    quit()
+
+    line = 'X  &((ORANGE & A)|BOY &   !THREE)|!this way&(five   | !(!one & two))'
+    expr = parse_expression(line, default_expressions)
+    print(expr)
+    variables = {
+        'X': False,
+        'ORANGE': True,
+        'A': True,
+        'BOY': True,
+        'THREE': True,
+        'this way': False,
+        'five': False,
+        'one': False,
+        'two': False,
+    }
+    print(expr.evaluate(variables))
---
+++ itemreader.py
@@ -0,0 +1,152 @@
+import struct
+
+MAP_SIZE = 100000
+MAP_ITEMS_OFFSET = 402700
+
+class Item(object):
+    def __init__(self, position, areaid, itemid):
+        self.areaid = areaid
+        self.position = position
+        self.itemid = itemid
+        self.name = None
+
+    def copy(self):
+        item = Item(self.position, self.areaid, self.itemid)
+        item.name = self.name
+        return item
+
+    def set_location(self, item):
+        self.areaid = item.areaid
+        self.position = item.position
+
+    def __str__(self):
+        x, y = self.position
+        return '(%d,%d) : %d : %d : %s' % (x, y, self.areaid, self.itemid, self.name)
+
+def parse_item_from_string(line):
+    pos, areaid, itemid, name = (s.strip() for s in line.split(':', 3))
+    import ast
+    item = Item(ast.literal_eval(pos), int(areaid), int(itemid))
+    if len(name) > 0 and name != 'None':
+        item.name = name
+    return item
+
+def to_position(index):
+    y = index%200
+    x = index//200
+    return x,y
+
+def to_index(position):
+    x, y = position
+    return x*200 + y
+
+def load_items(areaid):
+    f = open(map_filename(areaid), 'rb')
+    f.seek(MAP_ITEMS_OFFSET)
+    
+    tiledata = (struct.unpack('h', f.read(2))[0] for i in range(MAP_SIZE))
+    items = list(Item(to_position(pos), areaid, itemid) for pos, itemid in enumerate(tiledata) if itemid != 0)
+    f.close()
+    return list(items)
+
+def write_items(areaid, items):
+    tiledata = [b'\x00\x00' for i in range(MAP_SIZE)]
+    for item in items:
+        if item.areaid != areaid: continue
+        index = to_index(item.position)
+        tiledata[index] = struct.pack('h', item.itemid)
+
+    f = open(map_filename(areaid), 'r+b')
+    f.seek(MAP_ITEMS_OFFSET)
+    f.write(b''.join(tiledata))
+    f.close()
+
+def map_filename(areaid):
+    return 'area%d.map' % areaid
+
+def print_all_items():
+    sb = []
+    for areaid in range(0,10):
+        items = load_items(areaid)
+        sb.append('Area %d: NAME' % areaid)
+        for item in items:
+            sb.append(str(item))
+    print('\n'.join(sb))
+
+
+class ItemModifier(object):
+    def __init__(self, areaids):
+        self.areaids = list(areaids)
+        self.items = dict((areaid, {}) for areaid in areaids)
+        for areaid in areaids:
+            for item in load_items(areaid):
+                self.items[item.areaid][item.position] = item
+
+        self._set_all_dirty_flags(False)
+
+    def _set_all_dirty_flags(self, value):
+        self.modified = dict((areaid, value) for areaid in areaids)
+
+    def _dirty(self, areaid):
+        self.modified[areaid] = True
+
+    def clear_items(self):
+        self.items = dict((areaid, {}) for areaid in areaids)
+        self._set_all_dirty_flags(True)
+
+    def add_item(self, item):
+        self.items[item.areaid][item.position] = item
+        self._dirty(item.areaid)
+
+    def delete_item(self, item):
+        try: del self.items[item.areaid][item.position]
+        except KeyError:
+            print('item [%s] does not exist!' % item)
+        self._dirty(item.areaid)
+
+    def delete_position(self, areaid, position):
+        try: del self.items[areaid][position]
+        except KeyError:
+            print('position [%d, %s] does not exist!' % areaid, position)
+        self._dirty(item.areaid)
+
+    def save(self):
+        for areaid, modified in self.modified.items():
+            if not modified: continue
+            write_items(areaid, self.items[areaid].values())
+
+        # Reset dirty flags
+        self._set_all_dirty_flags(False)
+
+def revert_changes():
+    areaids = list(range(10))
+    import shutil
+    import os
+    BACKUP_DIR = 'backup_maps/'
+    for f in os.listdir(BACKUP_DIR):
+        shutil.copyfile(BACKUP_DIR+f, f)
+
+def item_modification():
+    areaids = list(range(10))
+    mod = ItemModifier(areaids)
+
+    mod.add_item(Item(
+        position=(140,99),
+        areaid=0,
+        itemid=28,
+    ))
+
+    mod.add_item(Item(
+        position=(141,99),
+        areaid=0,
+        itemid=28,
+    ))
+
+    mod.save()
+
+
+
+if __name__ == '__main__':
+    #revert_changes()
+    print_all_items()
+    #item_modification()

a98a0e727ca816021e04335ea2f1411f471e77a1
wcko87 <wcko87@gmail.com>
2017-08-20 02:43:30 +0800

    Fix background shuffle issues
    1. super trampoline bug in pyramid
    2. noah3 background messing with boss doors (noah3 background removed from shuffle)
    3. library room color removing trampolines from map (remove library room color from shuffle)

--- backgroundrandomizer.py
+++ backgroundrandomizer.py
@@ -1,35 +1,37 @@
 import random
 import time
 
+def to_tile_index(x, y):
+    return x*18 + y
+
 def shuffle_backgrounds(stored_datas):
     #start_time = time.time()
-    shuffler = Shuffler(stored_datas, lambda data : data.tiledata_roombg, filter_roombg)
+    shuffler = BackgroundShuffler(stored_datas)
     shuffler.shuffle()
     print('Backgrounds shuffled')
 
-    shuffler = Shuffler(stored_datas, lambda data : data.tiledata_roomcolor, filter_roomcolor)
+    shuffler = RoomColorShuffler(stored_datas)
     shuffler.shuffle()
     print('Tile colors shuffled')
     #print('Backgrounds shuffled in %f seconds' % (time.time()-start_time))
 
-def filter_roombg(val):
-    return val <= 118 and val not in (0,23,17,104)
 
-def filter_roomcolor(val):
-    return val <= 31 and val not in (0,5,6,30) # DLC: (0,5,32,34,55)
-
-class Shuffler(object):
-
-    def __init__(self, stored_datas, data_select, filter_function):
-        self.data_select = data_select
+class BackgroundShuffler(object):
+    def __init__(self, stored_datas):
         self.stored_datas = stored_datas
         original_locations = []
 
+        filter_function = self.filter_function
         for areaid, data in stored_datas.items():
             original_locations += ((areaid, posindex, val)
-                for posindex, val in enumerate(data_select(data)) if filter_function(val))
+                for posindex, val in enumerate(data.tiledata_roombg) if filter_function(val))
 
         self.original_locations = original_locations
+
+    def filter_function(self, val):
+        # don't shuffle DLC backgrounds
+        # don't shuffle Noah3 background because it does weird things to boss doors
+        return val <= 118 and val not in (0,23,17,104,110)
 
     def shuffle(self):
         backgrounds = list(set(val for areaid, posindex, val in self.original_locations))
@@ -37,7 +39,38 @@ class Shuffler(object):
         random.shuffle(new_backgrounds)
         allocation = dict(zip(backgrounds, new_backgrounds))
 
-        data_select = self.data_select
         stored_datas = self.stored_datas
         for areaid, posindex, val in self.original_locations:
-            data_select(stored_datas[areaid])[posindex] = allocation[val]
+            # Fix for pyramid super-trampoline bug
+            if areaid == 1 and posindex == to_tile_index(16,11): continue
+
+            stored_datas[areaid].tiledata_roombg[posindex] = allocation[val]
+
+
+class RoomColorShuffler(object):
+    def __init__(self, stored_datas):
+        self.stored_datas = stored_datas
+        original_locations = []
+
+        filter_function = self.filter_function
+        for areaid, data in stored_datas.items():
+            original_locations += ((areaid, posindex, val)
+                for posindex, val in enumerate(data.tiledata_roomcolor) if filter_function(val))
+
+        self.original_locations = original_locations
+
+    def filter_function(self, val):
+        # don't shuffle DLC colors
+        # don't shuffle library color (24) because it deletes trampolines
+        # don't shuffle FC2/HoM colors (6,30) because they do weird things to bosses
+        return val <= 31 and val not in (0,5,6,24,30) # DLC: (0,5,32,34,55)
+
+    def shuffle(self):
+        backgrounds = list(set(val for areaid, posindex, val in self.original_locations))
+        new_backgrounds = list(backgrounds)
+        random.shuffle(new_backgrounds)
+        allocation = dict(zip(backgrounds, new_backgrounds))
+
+        stored_datas = self.stored_datas
+        for areaid, posindex, val in self.original_locations:
+            stored_datas[areaid].tiledata_roomcolor[posindex] = allocation[val]
